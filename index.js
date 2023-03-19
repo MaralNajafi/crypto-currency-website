bbafa70697829a8bd0657d84263c916bc064db5d
Maral Najafi <marawlnajafi@gmail.com>
2023-03-19 22:38:28 +0330

    render data to dom

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const CRYPTO_CURRENCY_API_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+const CRYPTO_CURRENCY_API_URL =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
 async function fetchAPI(url) {
-    const data = await fetch (url);
-    const dataJson = await data.json();
-    return dataJson;
-};
-
+  const data = await fetch(url);
+  const dataJson = await data.json();
+  console.log(dataJson);
+  return dataJson;
+}
 
 let cryptoCurrenciesArray = [];
 
-document.addEventListener('DOMContentLoaded', async () => {
-    cryptoCurrenciesArray = await fetchAPI(CRYPTO_CURRENCY_API_URL).catch(console.log)
-    console.log(cryptoCurrenciesArray);
-})
+function updateDOM() {
+  let number = 0;
+  const cryptoCurrencies = cryptoCurrenciesArray
+    .map((cryptoCurrency) => {
+      number++;
+      return `
+            <tr>
+                <th scope="row">${number}</th>
+                <td>${cryptoCurrency.name}</td>
+                <td>${cryptoCurrency.symbol}</td>
+                <td>${cryptoCurrency.current_price}</td>
+            </tr>
+        `;
+    })
+    .join("");
+
+  document.getElementById("root").innerHTML = cryptoCurrencies;
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  cryptoCurrenciesArray = await fetchAPI(CRYPTO_CURRENCY_API_URL);
+  updateDOM();
+
+  //   refresh every 1 min
+  setInterval(async () => {
+    cryptoCurrenciesArray = await fetchAPI(CRYPTO_CURRENCY_API_URL);
+    updateDOM();
+  }, 60000);
+});
