f34ae912689df6a0165f7119b2557a2d3dc12f26
Maral Najafi <marawlnajafi@gmail.com>
2023-03-23 12:08:45 +0430

    feature: toggle favorite coin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let isFavorite = false;
 function updateDOM() {
   const cryptoCurrencies = cryptoCurrenciesArray
     .map((cryptoCurrency) => {
-      isFavorite = favoriteCoinsIDs.includes(cryptoCurrency.id)
+      isFavorite = favoriteCoinsIDs.includes(cryptoCurrency.id);
       return `
             <tr>
                 <td>
@@ -28,8 +28,10 @@ function updateDOM() {
                       height="16" 
                       fill=${isFavorite ? "#ffc107" : "currentColor"} 
                       class="bi bi-star" 
-                      style="cursor: ${isFavorite ? "default" : "pointer"};">
-                        <use style="pointer-events: none;" xlink:href=${isFavorite ? "#star-fill" : "#star"}></use>
+                      style="cursor: pointer";>
+                        <use style="pointer-events: none;" xlink:href=${
+                          isFavorite ? "#star-fill" : "#star"
+                        }></use>
                       </svg>
                     </button>
                   </div>
@@ -55,7 +57,9 @@ function updateDOM() {
                 </td>
                 <td class="text-center font-weight-bold">$${Number(
                   cryptoCurrency.current_price
-                ).toFixed(2).toLocaleString()}</td>
+                )
+                  .toFixed(2)
+                  .toLocaleString()}</td>
                 <td class="text-left ${
                   Number(cryptoCurrency.price_change_percentage_24h) < 0
                     ? " text-danger"
@@ -77,29 +81,35 @@ function updateDOM() {
 function addEventListenerToFavIcons() {
   const favIcons = document.querySelectorAll(".fav-icon");
   favIcons.forEach((favIcon) => {
-
     favIcon.addEventListener("click", (event) => {
       const favoriteCoinID = event.target.closest("button").dataset.id;
       isFavorite = favoriteCoinsIDs.includes(favoriteCoinID);
-      if(isFavorite){
-        return
-      }else{
+      const useTag = event.target.firstElementChild;
+      const svgTag = event.target.closest("svg");
+      if (isFavorite) {
+        const removeIndex = favoriteCoinsIDs.findIndex(
+          (id) => id === favoriteCoinID
+        );
+        removeFavoriteCoinToLocalStorage(removeIndex);
+        useTag.setAttribute("xlink:href", "#star");
+        svgTag.style.fill = "currentColor";
+        return;
+      } else {
         addFavoriteCoinToLocalStorage(favoriteCoinID);
-
-        const useTag = event.target.firstElementChild;
-        const svgTag = event.target.closest("svg");
         useTag.setAttribute("xlink:href", "#star-fill");
         svgTag.style.fill = "#ffc107";
-        svgTag.style.cursor = "default";
       }
-      
     });
-
   });
 }
 
 function addFavoriteCoinToLocalStorage(favoriteCoinID) {
   favoriteCoinsIDs.push(favoriteCoinID);
+  localStorage.setItem("favoriteCoinsIDs", favoriteCoinsIDs);
+}
+
+function removeFavoriteCoinToLocalStorage(removeIndex) {
+  favoriteCoinsIDs.splice(removeIndex, 1);
   localStorage.setItem("favoriteCoinsIDs", favoriteCoinsIDs);
 }
 
